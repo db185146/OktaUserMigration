879eb00c5b5cda72cbb0abf4c175f2ce4e6662f4
Daniel Bearden <daniel.bearden@ncr.com>
2022-02-16 11:03:24 -0600

    refactoring

--- OktaUserMig/Program.cs
+++ OktaUserMig/Program.cs
@@ -6,13 +6,10 @@ namespace OktaUserMig
 {
     class Program
     {
-        [JsonProperty("profile")]
-        private static User User;
-        
+       
         static void Main(string[] args)
         {
-            User = new User(DataAccess.LoadUserInfo("1"));
-            var result = OktaInterface.CreateUser(JsonConvert.SerializeObject(User));
+            var result = OktaInterface.CreateUser(JsonConvert.SerializeObject(new User(DataAccess.LoadUserInfo("1"))));
             Console.WriteLine($"Create user result\n{result}");
 
             result = OktaInterface.AuthenticateUser(JsonConvert.SerializeObject(new Authenticate(DataAccess.LoadUserInfo("1"))));
